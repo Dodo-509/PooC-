6e01aa0de7777e10b35cb4e1355c0dbf908c4354
Dodo-509 <adonaiclervil774@gmail.com>
2024-10-23 18:37:45 +0200

    Create Voiture.cpp

---
+++ Voiture.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include "Voiture.h"
+
+
+Voiture::Voiture () {
+	marque = "Undefined";
+	modele = "Undefined";
+	annee = 2000;
+	kilometrage = 0.0;
+	vitesse = 0.0;
+}
+
+Voiture::Voiture(std::string marque, std::modele, int annee, float kilometrage, float vitesse) {
+	this->marque = marque;
+	this->modele = modele;
+	this->annee = annee;
+	this->kilometrage = kilometrage;
+	this->vitesse = vitesse;
+}
+
+void Voiture::accelerer(float valeur) {
+	vitesse += valeur;
+}
+
+void Voiture::freiner(float valeur) {
+	if (vitesse >= valeur)
+		vitesse-=valeur;
+	else
+		vitesse = 0;
+}
+
+void Voiture::afficherInfo() {
+	cout<<"Marque      : "<<marque<<std::endl;
+	cout<<"Modele      : "<<modele<<std::endl;
+	cout<<"Annee       : "<<annee<<std::endl;
+	cout<<"Kilometrage : "<<kilometrage<<std::endl;
+	cout<<"Vitesse     : "<<vitesse<<std::endl;
+}
+
+void Voiture::avancer(float distance) {
+	kilometrage += distance;
+}
+
+Voiture::~Voiture() {
+	cout<<"La voiture est detruite"<<std::endl;
+}
