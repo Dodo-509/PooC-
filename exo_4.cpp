5b57b28441912eca0684493525c0a915eda05567
Dodo-509 <adonaiclervil774@gmail.com>
2024-11-03 19:28:47 +0100

    Update exo_4.cpp

--- exo_4.cpp
+++ exo_4.cpp
@@ -49,7 +49,7 @@ public:
     }
 
 
-    static vecteur3d somme(const vecteur3d &vect1, const vecteur3d &vect2) {
+    static vecteur3d somme_ref(const vecteur3d& vect1, const vecteur3d& vect2) {
         float x,y,z;
 
         x = vect1.getX() + vect2.getX();
@@ -59,14 +59,14 @@ public:
         vecteur3d v3(x,y,z);
         return v3;
     }
-    static vecteur3d somme(vecteur3d *v1, vecteur3d *v2) {
+    static vecteur3d* somme(vecteur3d *v1, vecteur3d *v2) {
         float x,y,z;
 
         x = v1->getX() + v2->getX();
         y = v1->getY() + v2->getY();
         z = v1->getZ() + v2->getZ();
 
-        vecteur3d v3(x,y,z);
+        vecteur3d *v3 = new vecteur3d (x,y,z);
         return v3;
     }
 
@@ -78,7 +78,7 @@ public:
         return v1.getX() * v2.getX() + v1.getY() * v2.getY() + v1.getZ() * v2.getZ();
     }
 
-    static float produit_scalaire(const vecteur3d &v1, const vecteur3d &v2) {
+    static float produit_scalaire_ref(const vecteur3d& v1, const vecteur3d& v2) {
         return v1.getX() * v2.getX() + v1.getY() * v2.getY() + v1.getZ() * v2.getZ();
     }
 
@@ -90,7 +90,7 @@ public:
         return v1.getX() == v2.getX() && v1.getY() == v2.getY() && v1.getZ() == v2.getZ();
     }
 
-    static bool coincide(const vecteur3d &v1, const vecteur3d &v2) {
+    static bool coincide_ref(const vecteur3d& v1, const vecteur3d& v2) {
         return v1.getX() == v2.getX() && v1.getY() == v2.getY() && v1.getZ() == v2.getZ();
     }
 
@@ -98,7 +98,7 @@ public:
         return v1->getX() == v2->getX() && v1->getY() == v2->getY() && v1->getZ() == v2->getZ();
     }
 
-    static vecteur3d normax(const vecteur3d v1, vecteur3d v2) {
+    static vecteur3d normax( vecteur3d v1, vecteur3d v2) {
         if(v1.norme() >= v2.norme())
             return v1;
         else
@@ -106,7 +106,7 @@ public:
 
     }
 
-    static vecteur3d normax(const vecteur3d &v1, const vecteur3d &v2) {
+    static vecteur3d normax_ref(const vecteur3d& v1, const vecteur3d& v2) {
         if(v1.norme() >= v2.norme())
             return v1;
         else
@@ -126,10 +126,14 @@ public:
 
 
 int main() {
-    vecteur3d v1(1.5, 2, 3.5), v2(1.3, 2.3, 1);
+    vecteur3d v1(1.3, 2.3, 1), v2(1.3, 2.3, 1);
     vecteur3d &ref_v1 = v1, &ref_v2 = v2;
-    vecteur3d v3 = vecteur3d::normax(ref_v1, ref_v2);
+    
+    
+    vecteur3d* v3 = vecteur3d::somme(&v1, &v2);
+    
+    cout<<v1.norme()<<endl<<vecteur3d::produit_scalaire(ref_v1, ref_v2)<<endl;
 
-    v3.affichage_composant();
+    v3->affichage_composant();
 return 0;
 }
